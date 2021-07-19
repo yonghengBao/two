6ef78a94e30a068fe0d1519f5dd509644ab26e39
baoyongheng <baoyongheng2009@163.com>
2021-07-19 14:30:27 +0800

    dev

--- src/utils/publicStyle.js
+++ src/utils/publicStyle.js
@@ -3,7 +3,7 @@ const cut = (theme) => {
   let cutcheack = theme=='dark'?true:false;
   document.getElementsByTagName("body")[0].style.setProperty("--backgroundColor", cutcheack ? "yellow" : "#fff");
   document.getElementsByTagName("body")[0].style.setProperty("--fonntColor", cutcheack ? "#cecece" : "#333");
-  document.getElementsByTagName("body")[0].style.setProperty("--backgroundMColor", cutcheack ? "#333333" : "#eee");
+  document.getElementsByTagName("body")[0].style.setProperty("--backgroundMColor", cutcheack ? "#333" : "#eee");
   document.getElementsByTagName("body")[0].style.setProperty("--tableColor", cutcheack ? "#000" : "#d8d8d8");
   document.getElementsByTagName("body")[0].style.setProperty("--tablesColor", cutcheack ? "#222" : "#fff");
   document.getElementsByTagName("body")[0].style.setProperty("--inputColor", cutcheack ? "#666" : "#fff");
